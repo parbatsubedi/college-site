efa6a811b526f86549a7a12cd698ca0267fb3191
Parbat Subedi <parbat.subedi@firstfintech.co>
2026-03-10 13:30:10 +0545

    fix: commented course seeder and events seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             SettingsSeeder::class,
-            CourseSeeder::class,
-            EventSeeder::class,
+            // CourseSeeder::class,
+            // EventSeeder::class,
             BannerSeeder::class,
         ]);
 
